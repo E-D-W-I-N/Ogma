331c91d1a64398b23d7476473b27de33902bf08f
E-D-W-I-N <scvaersoft503@gmail.com>
2020-01-03 15:30:31 +0700

    Update V2__Add_admin.sql

--- src/main/resources/db/migration/V2__Add_admin.sql
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -1,6 +1,6 @@
 insert into usr (id, username, password, email, first_name, last_name, phone, active)
-values (0, 'admin', '$2a$08$mTApv7MZDEZrmXT/I/dtNuxgTapu6Ck7sOL0xC.0cO9cluwzsEeK.', 'kaluginnikita1998@mail.ru',
-        'Nikita', 'Kalugin', '+7-951-607-9856', true);
+values (0, 'admin', '$2a$08$mTApv7MZDEZrmXT/I/dtNuxgTapu6Ck7sOL0xC.0cO9cluwzsEeK.', 'test@mail.ru',
+        'Nikita', 'Kalugin', '8-800-555-3535', true);
 
 insert into user_role (user_id, roles)
 values (0, 'USER'),
